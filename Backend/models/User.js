075549b1e6c9197a15b24d46768b24dbb14b5094
Michal Danielczuk <michaldanielczuk@wp.pl>
2020-04-20 17:45:53 +0200

    Inicjalizacja projektu

---
+++ Backend/models/User.js
@@ -0,0 +1,38 @@
+const Sequelize = require("sequelize")
+const db = require("../database/db")
+
+module.exports = db.sequelize.define(
+    'user',
+    {
+        iduser: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        firstname: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        lastname: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        phonenumber: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    },
+    {
+        timestamps: false
+    }
+)
+
+
